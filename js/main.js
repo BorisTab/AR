531ad2d6ddd5cb1dfc8d5a6e8d55459c09e7c3f8
BorisTab <boristab23@gmail.com>
2019-01-13 12:30:43 +0300

    max size of rectangle

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-var rectangle;
-var map;
-var rightUpLat;
-var rightUpLng;
-var leftDownLat;
-var leftDownLng;
+let rectangle;
+let map;
+let rightUpLat;
+let rightUpLng;
+let leftDownLat;
+let leftDownLng;
+let maxLength = 0.5;
+let maxWidth = 0.5;
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 55.72, lng: 37.66},
-        zoom: 11
+        center: {lat: 55.73, lng: 37.66},
+        zoom: 13
     });
 
-    var bounds = {
-        north: 55.8,
-        south: 55.7,
-        east: 37.75,
-        west: 37.55
+    let bounds = {
+        north: 55.75,
+        south: 55.72,
+        east: 37.70,
+        west: 37.65
     };
 
     // Define the rectangle and set its editable property to true.
@@ -34,14 +37,64 @@ function initMap() {
 // Show the new coordinates for the rectangle in an info window.
 
 function getCoordinate(event) {
-    var ne = rectangle.getBounds().getNorthEast();
-    var sw = rectangle.getBounds().getSouthWest();
+    let ne = rectangle.getBounds().getNorthEast();
+    let sw = rectangle.getBounds().getSouthWest();
 
     rightUpLat = ne.lat();
     rightUpLng = ne.lng();
     leftDownLat = sw.lat();
     leftDownLng = sw.lng();
+
+    let rectangleLength = rightUpLng - leftDownLng;
+    let rectangleWidth = rightUpLat - leftDownLat;
+    let check = false;
+
+    if (rectangleWidth > maxWidth) {
+
+        let newBounds = {
+            north: rightUpLat,
+            south: rightUpLat - maxWidth + 0.02,
+            east: rightUpLng,
+            west: leftDownLng
+        };
+
+        rectangle.setBounds(newBounds);
+        check = true;
+    }
+
+    if (rectangleLength > maxLength) {
+
+        let newBounds = {
+            north: rightUpLat,
+            south: leftDownLat,
+            east: rightUpLng,
+            west: rightUpLng - maxLength + 0.02
+        };
+
+        rectangle.setBounds(newBounds);
+        check = true;
+    }
+
+
+    if (check) {
+        alert('Превышен максимальный размер области');
+    }
 }
+
+function preloader(){
+    $(() => {
+        let preload =$('.preloader');
+        setInterval(() => {
+            preload.css('opacity', 0);
+            setInterval(
+                () => preload.remove(),
+                parseInt(preload.css('--duration'))*1000
+            );
+
+        },1000);
+    });
+}
+
 $('document').ready(function () {
     $(".coordinate-send").submit(function() {
         $('#right-up-lat').val(rightUpLat);
@@ -55,11 +108,11 @@ $('document').ready(function () {
             url: "",              //напишешь сюда обработчик
             data: th.serialize()
         }).done(function() {
-            $('.choose-zone-title').text('Спасибо!');
-            $('.coordinate-send').css('display', 'none');
+            $('.choose-zone-title').text('Вы может отправить новую зону');
+            alert('Зона отправлена');
             setTimeout(function() {
                 th.trigger("reset");
-            }, 1000);
+            }, 100);
         });
         return false;
     });
